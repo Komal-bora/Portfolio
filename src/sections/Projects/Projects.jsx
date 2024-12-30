cf2ce10919b95afab50b711c3d7b20381b3ee6fe
Your Name <you@example.com>
2024-12-30 12:09:17 +0530

    added a new project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import sortingVisualizer from '../../assets/sorting-visualizer.jpg';
 import ProjectCard from '../../common/ProjectCard';
 import tiffinService from '../../assets/tiffin-service.png';
 import games from '../../assets/games.jpeg';
-import imageSlider from '../../assets/image-slider.jpeg';
+import notesApp from '../../assets/notes-img.jpg';
 
 function Projects() {
   return (
@@ -13,7 +13,7 @@ function Projects() {
         <ProjectCard src={sortingVisualizer} link="https://komal-bora.github.io/Sorting-Visualizer/" h3="Sorting Visualizer" p="Visualize sorting algorithms" />
         <ProjectCard src={tiffinService} link="https://komal-bora.github.io/MotionCut/Project4_Tiffin-service-website/" h3="Tiffin Service " p="Home-cooked meals" />
         <ProjectCard src={games} link="#" h3="Fun Blend Fusion" p={"Variety games, \nendless fun "} />
-        <ProjectCard src={imageSlider} link="https://komal-bora.github.io/MotionCut/Project2_dynamic-image-slider/" h3="SlideVista" p="Dynamic image slider" />
+        <ProjectCard src={notesApp} link="https://my-notes-app.netlify.app/" h3="My Notes App" p="Note taking application" />
       </div>
     </section>
   )
