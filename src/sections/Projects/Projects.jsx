548c258da6d10122b2a239fec1c9cd1904044c66
Your Name <you@example.com>
2025-02-15 23:47:20 +0530

    projects updated

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,7 +2,7 @@ import styles from './ProjectStyles.module.css';
 import sortingVisualizer from '../../assets/sorting-visualizer.jpg';
 import ProjectCard from '../../common/ProjectCard';
 import tiffinService from '../../assets/tiffin-service.png';
-import games from '../../assets/games.jpeg';
+import productPage from '../../assets/product.jpg';
 import notesApp from '../../assets/notes-img.jpg';
 
 function Projects() {
@@ -10,10 +10,10 @@ function Projects() {
     <section id="projects" className={styles.container}>
       <h1 className='sectionTitle'>Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard src={sortingVisualizer} link="https://komal-bora.github.io/Sorting-Visualizer/" h3="Sorting Visualizer" p="Visualize sorting algorithms" />
-        <ProjectCard src={tiffinService} link="https://komal-bora.github.io/MotionCut/Project4_Tiffin-service-website/" h3="Tiffin Service " p="Home-cooked meals" />
-        <ProjectCard src={games} link="#" h3="Fun Blend Fusion" p={"Variety games, \nendless fun "} />
+        <ProjectCard src={productPage} link="https://product-page-pern.onrender.com/" h3="Product Page" p={"Add, View, Update & \nDelete Products Easily!"} />
         <ProjectCard src={notesApp} link="https://my-notes-app.netlify.app/" h3="My Notes App" p="Note taking application" />
+        <ProjectCard src={tiffinService} link="https://komal-bora.github.io/MotionCut/Project4_Tiffin-service-website/" h3="Tiffin Service " p="Home-cooked meals" />
+        <ProjectCard src={sortingVisualizer} link="https://komal-bora.github.io/Sorting-Visualizer/" h3="Sorting Visualizer" p="Visualize sorting algorithms" />
       </div>
     </section>
   )
